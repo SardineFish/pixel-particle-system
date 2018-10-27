db52d31ffaad77864976ba447c2fc33476c9a045
SardineFish <Sardine@live.cn>
2018-10-27 16:06:28 +0800

    Add basic math libs.

---
+++ src/lib.ts
@@ -0,0 +1,47 @@
+import { Vector4 } from "./vector";
+
+export class Color
+{
+    red: number = 255;
+    green: number = 255;
+    blue: number = 255;
+    alpha: number = 1.0;
+    constructor(r: number, g: number, b: number, a: number = 1.0)
+    {
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+        this.alpha = a;
+    }
+
+    static add(a: Color, b: Color)
+    {
+        const t = b.alpha;
+        return new Color(
+            (1 - t) * a.red + t * b.red,
+            (1 - t) * a.green + t * b.green,
+            (1 - t) * a.blue + t * b.blue,
+            1 - (1 - a.alpha) * (1 - b.alpha)
+        );
+    }
+
+    static blend(a: Color, b: Color, t: number)
+    {
+        return new Color(
+            (1 - t) * a.red + t * b.red,
+            (1 - t) * a.green + t * b.green,
+            (1 - t) * a.blue + t * b.blue,
+            1
+        );
+    }
+
+    toString()
+    {
+        return `rgba(${this.red}, ${this.green}, ${this.blue}, ${this.alpha})`;
+    }
+    toVector4(): Vector4
+    {
+        return new Vector4(this.red / 255, this.green / 255, this.blue / 255, this.alpha);
+    }
+}
+
---
+++ src/math.ts
@@ -0,0 +1,180 @@
+export class Vector2 extends Array<number>
+{
+    constructor(v: number[])
+    constructor(x: number, y: number)
+    constructor(x: number | number[], y?: number)
+    {
+        if (x instanceof Array)
+            super(...x);
+        else
+            super(x, y);
+    }
+
+    get x(): number { return this[0]; }
+    set x(value) { this[0] = value; }
+    get y(): number { return this[1]; }
+    set y(value) { this[1] = value; }
+
+    get normalized()
+    {
+        let l = Math.hypot(this.x, this.y);
+        return new Vector2(this.x / l, this.y / l);
+    }
+    get magnitude()
+    {
+        return Math.hypot(this.x, this.y);
+    }
+}
+
+export class Vector4 extends Array<number>
+{
+    constructor(v: number[])
+    constructor(x: number, y: number, z: number, w: number)
+    constructor(x: number | number[], y?: number, z?: number, w?: number)
+    {
+        if (x instanceof Array)
+            super(...x);
+        else
+            super(x, y, z, w);
+    }
+
+    get x(): number { return this[0]; }
+    set x(value) { this[0] = value; }
+    get y(): number { return this[1]; }
+    set y(value) { this[1] = value; }
+    get z(): number { return this[2]; }
+    set z(value) { this[2] = value; }
+    get w(): number { return this[3]; }
+    set w(value) { this[3] = value; }
+
+    get normalized()
+    {
+        return new Vector4(this.x / this.magnitude, this.y / this.magnitude, this.z / this.magnitude, this.w / this.magnitude);
+    }
+    get magnitude()
+    {
+        return Math.hypot(this.x, this.y, this.z, this.w);
+    }
+}
+
+function vec4(x: number, y: number, z: number, w: number): Vector4
+{
+    return new Vector4(x, y, z, w);
+}
+
+export function vec2(x: number, y: number): Vector2
+{
+    return new Vector2(x, y);
+}
+export function plus(u: Vector2, v: Vector2): Vector2
+export function plus(u: Vector4, v: Vector4): Vector4
+export function plus(u: Vector4 | Vector2, v: Vector4 | Vector2): Vector4 | Vector2
+{
+    if (u instanceof Vector2)
+    {
+        return new Vector2(u.x + v.x, u.y + v.y);
+    }
+    else if (v instanceof Vector4)
+    {
+        return new Vector4(u.x + v.x, u.y + v.y, u.z + v.z, u.w + v.w);
+    }
+}
+export function minus(u: Vector2, v: Vector2): Vector2
+export function minus(u: Vector4, v: Vector4): Vector4
+export function minus(u: Vector4 | Vector2, v: Vector4 | Vector2): Vector4 | Vector2
+{
+    if (u instanceof Vector2)
+    {
+        return new Vector2(u.x - v.x, u.y - v.y);
+    }
+    else if (v instanceof Vector4)
+    {
+        return new Vector4(u.x - v.x, u.y - v.y, u.z - v.z, u.w - v.w);
+    }
+}
+export function scale(u: Vector2, k: number): Vector2
+export function scale(u: Vector4, k: number): Vector4
+export function scale(u: Vector4 | Vector2, k: number): Vector4 | Vector2
+{
+    if (u instanceof Vector2)
+    {
+        return new Vector2(u.x * k, u.y * k);
+    }
+    else
+    {
+        return new Vector4(u.x * k, u.y * k, u.z * k, u.w * k);
+    }
+}
+export function dot(u: Vector2, v: Vector2): number
+export function dot(u: Vector4, v: Vector4): number
+export function dot(u: Vector4 | Vector2, v: Vector4 | Vector2): number
+{
+    if (u instanceof Vector2)
+    {
+        return u.x * v.x + u.y * v.y;
+    }
+    else if (v instanceof Vector4)
+    {
+        return u.x * v.x +
+            u.y * v.y +
+            u.z * v.z +
+            u.w * v.w;
+    }
+}
+
+export function cross(u: Vector2, v: Vector2): number
+{
+    return u.x * v.y - u.y * v.x;
+}
+
+class Matrix3x3
+{
+    constructor(mat: Matrix3x3 | number[][] = null)
+    {
+        this[0] = [1, 0, 0];
+        this[1] = [0, 1, 0];
+        this[2] = [0, 0, 1];
+        if (mat instanceof Matrix3x3 || mat instanceof Array)
+        {
+            this[0] = mat[0].copyWithin(0, 0);
+            this[1] = mat[1].copyWithin(0, 0);
+            this[2] = mat[2].copyWithin(0, 0);
+        }
+    }
+    [indexer: number]: number[];
+    static get identity()
+    {
+        return new Matrix3x3();
+    }
+    static multipleVector(mat: Matrix3x3, v: Vector2): Vector2
+    {
+        let result = [
+            mat[0][0] * v[0] + mat[0][1] * v[1] + mat[0][2] * 1,
+            mat[1][0] * v[0] + mat[1][1] * v[1] + mat[1][2] * 1,
+            mat[2][0] * v[2] + mat[2][1] * v[1] + mat[2][2] * 1,
+        ]
+        return new Vector2(result);
+    }
+    static multipleMatrix(a: Matrix3x3, b: Matrix3x3): Matrix3x3
+    {
+        let mat = new Matrix3x3();
+        for (let i = 0; i < 3; i++)
+            for (let j = 0; j < 3; j++)
+                for (let k = 0; k < 3; k++)
+                    mat[i][j] = a[i][k] * b[k][j];
+        return mat;
+    }
+
+    multipleVector(v: Vector2): Vector2
+    {
+        return Matrix3x3.multipleVector(this, v);
+    }
+    multipleMatrix(m: Matrix3x3): Matrix3x3
+    {
+        let mat = Matrix3x3.multipleMatrix(this, m);
+        this[0] = mat[0].copyWithin(0, 0);
+        this[1] = mat[1].copyWithin(0, 0);
+        this[2] = mat[2].copyWithin(0, 0);
+        return this;
+    }
+}
